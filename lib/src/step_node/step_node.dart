b44519215bed3b997a8c0ecc8513fa110087e78d
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-03 12:02:39 +0330

    chore: initialize step node core widget

--- lib/src/step_node/step_node.dart
+++ lib/src/step_node/step_node.dart
@@ -1,27 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:step_progress/src/step_node/step_node_shape.dart';
 
 class StepNode extends StatelessWidget {
   const StepNode({
-    required this.radius,
+    required this.width,
+    required this.height,
     required this.deaultBackgroundColor,
     required this.activeBackgroundColor,
     required this.shape,
     super.key,
   });
 
-  final double radius;
+  final double width;
+  final double height;
   final Color deaultBackgroundColor;
   final Color activeBackgroundColor;
-  final BoxShape shape;
+  final StepNodeShape shape;
+
+
+  // just assume that we use circle mode
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: radius,
-      height: radius,
+      width: width,
+      height: width,
       decoration: BoxDecoration(
         color: deaultBackgroundColor,
-        shape: shape,
+        shape: BoxShape.circle,
       ),
     );
   }
---
+++ lib/src/step_node/step_node_core.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class StepNodeCore extends StatelessWidget {
+  const StepNodeCore({
+    required this.icon,
+    required this.iconColor,
+    required this.backgroundColor,
+    super.key,
+  });
+
+  final Widget icon;
+  final Color iconColor;
+  final Color backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
