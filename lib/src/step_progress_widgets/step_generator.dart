418462c6002b287965766c445874294f51ce65f6
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-08 00:29:08 +0330

    docs: add documentation for the StepGenerator widget

--- lib/src/step_progress_widgets/step_generator.dart
+++ lib/src/step_progress_widgets/step_generator.dart
@@ -4,6 +4,31 @@ import 'package:step_progress/src/step_node/step_node.dart';
 import 'package:step_progress/src/step_node/step_node_ripple.dart';
 import 'package:step_progress/src/step_progress_theme.dart';
 
+/// A widget that generates a step in a step progress indicator.
+///
+/// The [StepGenerator] widget is used to create a step with customizable
+/// width, height, active state, axis, title, and subtitle.
+///
+/// The [width] and [height] parameters are required to define the size of the
+/// step.
+/// The [isActive] parameter indicates whether the step is active or not, with
+/// a default value of false.
+/// The [axis] parameter specifies the orientation of the step, either
+/// horizontal or vertical, with a default value of [Axis.horizontal].
+/// The [title] and [subTitle] parameters are optional and can be used to
+/// display additional information about the step.
+///
+/// Example usage:
+/// ```dart
+/// StepGenerator(
+///   width: 50.0,
+///   height: 50.0,
+///   isActive: true,
+///   axis: Axis.vertical,
+///   title: 'Step 1',
+///   subTitle: 'Introduction',
+/// )
+/// ```
 class StepGenerator extends StatelessWidget {
   const StepGenerator({
     required this.width,
@@ -15,13 +40,46 @@ class StepGenerator extends StatelessWidget {
     super.key,
   });
 
+  /// Indicates whether the step is active.
   final bool isActive;
+
+  /// The axis along which the step is oriented.
   final Axis axis;
+
+  /// The width of the step.
   final double width;
+
+  /// The height of the step.
   final double height;
+
+  /// The title of the step.
   final String? title;
+
+  /// The subtitle of the step.
   final String? subTitle;
 
+  /// Builds a widget that represents a step in a step progress indicator.
+  ///
+  /// The widget can be displayed either vertically or horizontally based on the
+  /// `axis` property. It supports an optional ripple effect around the step
+  /// node and can display a title and subtitle.
+  ///
+  /// The appearance of the step node, label, and ripple effect is determined by
+  /// the theme data provided by `StepProgressTheme`.
+  ///
+  /// - If `axis` is `Axis.vertical`, the step node and label are arranged in a
+  /// row.
+  /// - If `axis` is `Axis.horizontal`, the step node and label are arranged in
+  /// a column.
+  /// - If `enableRippleEffect` is true, a ripple effect is displayed around the
+  /// step node.
+  ///
+  /// The widget uses the following properties from the theme data:
+  /// - `enableRippleEffect`: Determines if the ripple effect is enabled.
+  /// - `labelStyle`: The style for the step label.
+  /// - `stepNodeStyle`: The style for the step node.
+  /// - `shape`: The shape of the step node.
+  /// - `rippleEffectStyle`: The style for the ripple effect.
   @override
   Widget build(BuildContext context) {
     final themeData = StepProgressTheme.of(context)!.data;
