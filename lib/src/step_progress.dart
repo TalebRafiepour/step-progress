a080f04519e95fc564290ee92f62c371190b4bc3
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-08 00:04:38 +0330

    docs: add more documentation for StepProgress widget class

--- lib/src/step_progress.dart
+++ lib/src/step_progress.dart
@@ -8,8 +8,48 @@ import 'package:step_progress/step_progress.dart';
 /// The [currentIndex] parameter indicates the index of the current step.
 typedef OnStepChanged = void Function(int currentIndex);
 
+/// A typedef for a callback function that is triggered when a step is tapped.
+///
+/// The callback function takes an integer [index] as a parameter, which
+/// represents the index of the step that was tapped.
 typedef OnStepTapped = void Function(int index);
 
+/// A widget that displays a step progress indicator.
+///
+/// The [StepProgress] widget is a customizable step progress indicator that
+/// can be used to show the progress of a multi-step process.
+///
+/// The [totalSteps] parameter is required and specifies the total number of
+/// steps in the progress indicator.
+///
+/// The [controller] parameter can be used to control the progress of the steps.
+///
+/// The [currentStep] parameter specifies the current step in the progress
+/// indicator. It defaults to 0.
+///
+/// The [stepSize] parameter specifies the size of each step. It defaults to 48.
+///
+/// The [theme] parameter specifies the theme data for the step progress
+/// indicator. It defaults to [StepProgressThemeData].
+///
+/// The [margin] parameter specifies the margin around the step progress
+/// indicator. It defaults to [EdgeInsets.zero].
+///
+/// The [padding] parameter specifies the padding inside the step progress
+/// indicator. It defaults to [EdgeInsets.all(4)].
+///
+/// The [axis] parameter specifies the axis along which the steps are laid out.
+/// It defaults to [Axis.horizontal].
+///
+/// The [titles] parameter can be used to specify titles for each step.
+///
+/// The [subTitles] parameter can be used to specify subtitles for each step.
+///
+/// The [onStepTapped] parameter is a callback that is called when a step is
+/// tapped.
+///
+/// The [onStepChanged] parameter is a callback that is called when the current
+/// step changes.
 class StepProgress extends StatefulWidget {
   const StepProgress({
     required this.totalSteps,
@@ -50,20 +90,28 @@ class StepProgress extends StatefulWidget {
           ' both are provided',
         );
 
+  /// Titles for each step in the progress
   final List<String>? titles;
 
+  /// Subtitles for each step in the progress
   final List<String>? subTitles;
 
+  /// Size of each step indicator
   final double stepSize;
 
+  /// Total number of steps in the progress
   final int totalSteps;
 
+  /// Current step in the progress
   final int currentStep;
 
+  /// Theme data for customizing the step progress appearance
   final StepProgressThemeData theme;
 
+  /// Axis along which the steps are arranged (horizontal or vertical)
   final Axis axis;
 
+  /// Callback function when a step is tapped
   final OnStepTapped? onStepTapped;
 
   /// The controller that manages the state and behavior of the step progress.
@@ -100,6 +148,9 @@ class _StepProgressState extends State<StepProgress>
     super.initState();
   }
 
+  /// Called whenever the widget configuration changes.
+  /// This method changes the current step when it's changed in the parent 
+  /// widget by setState.
   @override
   void didUpdateWidget(covariant StepProgress oldWidget) {
     if (widget.currentStep != _currentStep ||
@@ -109,12 +160,30 @@ class _StepProgressState extends State<StepProgress>
     super.didUpdateWidget(oldWidget);
   }
 
+  /// Returns the current step of the progress.
+  /// 
+  /// If a [StepProgressController] is provided, it retrieves the current step
+  ///  from the controller. Otherwise, it uses the `currentStep` 
+  /// property of the widget.
+  /// 
+  /// This getter ensures that the current step is always accurately retrieved 
+  /// based on the presence of a controller.
   int get _getCurrentStep {
     return widget.controller != null
         ? widget.controller!.currentStep
         : widget.currentStep;
   }
 
+  /// Changes the current step to the specified [newStep].
+  ///
+  /// If [newStep] is the same as the current step, less than 0, or greater than
+  /// or equal to the total number of steps, the function will return without
+  /// making any changes.
+  ///
+  /// When the step is successfully changed, the state is updated and the
+  /// `onStepChanged` callback is called with the new step value.
+  ///
+  /// [newStep] - The step to change to.
   void _changeStep(int newStep) {
     if (_currentStep == newStep ||
         newStep < 0 ||
@@ -131,6 +200,7 @@ class _StepProgressState extends State<StepProgress>
   Widget build(BuildContext context) {
     return StepProgressTheme(
       data: widget.theme,
+      // To fill available space 
       child: Flexible(
         flex: 10,
         child: Align(
