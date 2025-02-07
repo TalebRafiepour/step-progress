674e779ae3fb93da46249c9c7f9af44d9d4f9f83
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-08 00:07:34 +0330

    docs: add documentation for StepProgressTheme class

--- lib/src/step_progress_theme.dart
+++ lib/src/step_progress_theme.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:step_progress/step_progress.dart';
 
+/// A widget that provides theme data to descendant widgets.
+///
+/// This widget extends [InheritedWidget] and allows you to pass
+/// [StepProgressThemeData] to its descendants. The theme data can be
+/// accessed using `StepProgressTheme.of(context)`.
+///
+/// The [child] parameter is the widget below this widget in the tree.
+/// The [data] parameter is the theme data that will be passed to the
+/// descendants.
+///
+/// Example usage:
+/// ```dart
+/// StepProgressTheme(
+///   data: StepProgressThemeData(...),
+///   child: MyWidget(),
+/// )
+/// ```
+///
+/// See also:
+/// - [InheritedWidget]
+/// - [StepProgressThemeData]
 class StepProgressTheme extends InheritedWidget {
   const StepProgressTheme({
     required super.child,
@@ -8,6 +29,7 @@ class StepProgressTheme extends InheritedWidget {
     super.key,
   });
 
+  /// The theme data for the step progress widget.
   final StepProgressThemeData data;
 
   @override
