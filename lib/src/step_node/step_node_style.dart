1e53ab883f60f12c3a692a68efd14d72560d463e
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-07 23:39:58 +0330

    docs: write documentation for StepNodeStyle class

--- lib/src/step_node/step_node_style.dart
+++ lib/src/step_node/step_node_style.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:step_progress/src/step_node/step_node_shape.dart';
 
+/// A class that defines the style for a step node in a step progress indicator.
+///
+/// The [StepNodeStyle] class allows customization of various aspects of the
+/// step node's appearance, including colors, shapes, icons, and animations.
+///
+/// The following properties can be customized:
+///
+/// * [defaultForegroundColor]: The default color of the step node's foreground.
+/// * [activeForegroundColor]: The color of the step node's foreground when it
+/// is active.
+/// * [animationDuration]: The duration of the animation when the step node
+/// changes state.
+/// * [shape]: The shape of the step node. Defaults to [StepNodeShape.circle].
+/// * [decoration]: The decoration of the step node. Defaults to a
+/// [BoxDecoration] with a semi-transparent white color.
+/// * [icon]: The icon to display inside the step node.
+/// * [activeIcon]: The icon to display inside the step node when it is active.
+/// Defaults to a check icon.
+/// * [iconColor]: The color of the icon inside the step node. Defaults to a
+/// teal color.
+/// * [enableRippleEffect]: Whether to enable the ripple effect on the step
+/// node. Defaults to true.
 class StepNodeStyle {
   const StepNodeStyle({
     this.defaultForegroundColor,
@@ -20,16 +42,50 @@ class StepNodeStyle {
     this.enableRippleEffect = true,
   });
 
+  /// The default color for the foreground of the step node.
+  /// If not set, it will be read from the theme.
   final Color? defaultForegroundColor;
+
+  /// The color for the foreground of the step node when it is active.
+  /// If not set, it will be read from the theme.
   final Color? activeForegroundColor;
-  final Color iconColor;
-  final StepNodeShape shape;
-  final BoxDecoration decoration;
-  final Widget? icon;
-  final Widget? activeIcon;
-  final bool enableRippleEffect;
+
+  /// The duration of the animation for the step node.
+  /// If not set, it will be read from the theme.
   final Duration? animationDuration;
 
+  /// The color of the icon.
+  final Color iconColor;
+
+  /// The shape of the step node.
+  final StepNodeShape shape;
+
+  /// The decoration of the step node.
+  final BoxDecoration decoration;
+
+  /// The icon widget to display.
+  final Widget? icon;
+
+  /// The icon widget to display when the step is active.
+  final Widget? activeIcon;
+
+  /// Whether to enable the ripple effect.
+  final bool enableRippleEffect;
+
+  /// Creates a copy of this [StepNodeStyle] but with the given fields replaced
+  /// with the new values.
+  ///
+  /// If a field is not provided, the value from the current instance is used.
+  ///
+  /// - [defaultForegroundColor]: The default color for the foreground.
+  /// - [activeForegroundColor]: The color for the foreground when active.
+  /// - [iconColor]: The color of the icon.
+  /// - [shape]: The shape of the step node.
+  /// - [decoration]: The decoration for the step node.
+  /// - [icon]: The icon widget.
+  /// - [activeIcon]: The icon widget when active.
+  /// - [enableRippleEffect]: Whether to enable the ripple effect.
+  /// - [animationDuration]: The duration of the animation.
   StepNodeStyle copyWith({
     Color? defaultForegroundColor,
     Color? activeForegroundColor,
