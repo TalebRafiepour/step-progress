d7a0a1bfc5e593dab715da933bd87d82e1d3eaaa
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-08 11:34:39 +0330

    docs: add documentation for the StepNodeRipple widget class

--- lib/src/step_node/step_node_ripple.dart
+++ lib/src/step_node/step_node_ripple.dart
@@ -4,6 +4,38 @@ import 'package:step_progress/src/step_node/step_node_shape.dart';
 import 'package:step_progress/src/step_node/step_node_shaped_container.dart';
 import 'package:step_progress/src/step_progress_theme.dart';
 
+/// A widget that displays a ripple effect around a step node.
+///
+/// The [StepNodeRipple] widget is used to create a ripple effect around a step
+/// node with customizable shape, size, style, and visibility.
+///
+/// The ripple effect can be customized using the [style] parameter, which
+/// accepts a [RippleEffectStyle] object. The number of ripples can be
+/// controlled using the [count] parameter, and the visibility of the ripple
+/// effect can be toggled using the [isVisible] parameter.
+///
+/// The [stepNodeShape] parameter defines the shape of the step node, while the
+/// [width] and [height] parameters define the size of the ripple effect.
+///
+/// Example usage:
+/// ```dart
+/// StepNodeRipple(
+///   stepNodeShape: StepNodeShape.circle,
+///   width: 50.0,
+///   height: 50.0,
+///   style: RippleEffectStyle(color: Colors.blue),
+///   count: 4,
+///   isVisible: true,
+/// )
+/// ```
+///
+/// Parameters:
+/// - [stepNodeShape]: The shape of the step node.
+/// - [width]: The width of the ripple effect.
+/// - [height]: The height of the ripple effect.
+/// - [style]: The style of the ripple effect.
+/// - [count]: The number of ripples. Defaults to 6.
+/// - [isVisible]: Whether the ripple effect is visible. Defaults to true.
 class StepNodeRipple extends StatelessWidget {
   const StepNodeRipple({
     required this.stepNodeShape,
@@ -15,11 +47,22 @@ class StepNodeRipple extends StatelessWidget {
     super.key,
   });
 
+  /// The shape of the step node.
   final StepNodeShape stepNodeShape;
+
+  /// The width of the ripple effect.
   final double width;
+
+  /// The height of the ripple effect.
   final double height;
+
+  /// The style of the ripple effect.
   final RippleEffectStyle style;
+
+  /// The number of ripple effects.
   final int count;
+
+  /// Whether the ripple effect is visible.
   final bool isVisible;
 
   @override
