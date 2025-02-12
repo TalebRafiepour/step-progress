83bafeb158f43187f32cbaa96a27c91ed64ea838
TalebRafiepour <taleb_r75@yahoo.com>
2025-02-12 15:57:15 +0330

    refactor: update the example one code

--- example/lib/example_one.dart
+++ example/lib/example_one.dart
@@ -1,96 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:step_progress/step_progress.dart';
 
-class ExampleOne extends StatefulWidget {
+class ExampleOne extends StatelessWidget {
   const ExampleOne({super.key});
 
   @override
-  _ExampleOneState createState() => _ExampleOneState();
-}
-
-class _ExampleOneState extends State<ExampleOne> {
-  final PageController _pageController = PageController();
-  final StepProgressController _stepProgressController =
-      StepProgressController(totalSteps: 4);
-
-  @override
   Widget build(BuildContext context) {
+    final StepProgressController stepProgressController =
+        StepProgressController(totalSteps: 4);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Example One'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          StepProgress(
-            totalSteps: 4,
-            controller: _stepProgressController,
-            axis: Axis.vertical,
-            titles: const [
-              'step 1',
-              'step 2',
-              'step 3',
-              'step 4',
-            ],
-            onStepChanged: (index) {
-              debugPrint('on step changed: $index');
-            },
-            onStepNodeTapped: (index) {
-              debugPrint('on step tapped with index: $index');
-            },
-          ),
-          Expanded(
-            child: PageView(
-              controller: _pageController,
+      body: SafeArea(
+        child: Column(
+          spacing: 48,
+          children: [
+            StepProgress(
+              totalSteps: 4,
+              controller: stepProgressController,
+              axis: Axis.vertical,
+              titles: const [
+                'step 1',
+                'step 2',
+                'step 3',
+                'step 4',
+              ],
+              onStepChanged: (index) {
+                debugPrint('on step changed: $index');
+              },
+              onStepNodeTapped: (index) {
+                debugPrint('on step tapped with index: $index');
+              },
+              onStepLineTapped: (index) {
+                debugPrint('on step line tapped with index: $index');
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 38,
               children: [
-                Container(
-                  color: Colors.white,
+                ElevatedButton(
+                  onPressed: stepProgressController.previousStep,
+                  child: const Text('Prev'),
                 ),
-                Container(
-                  color: Colors.grey[100],
-                ),
-                Container(
-                  color: Colors.grey[300],
-                ),
-                Container(
-                  color: Colors.grey[500],
+                ElevatedButton(
+                  onPressed: stepProgressController.nextStep,
+                  child: const Text('Next'),
                 ),
               ],
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: Align(
-        alignment: Alignment.bottomCenter,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                _pageController.previousPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInCubic,
-                );
-                _stepProgressController.previousStep();
-              },
-              tooltip: 'Back',
-              heroTag: 'Back',
-              child: const Text('Back'),
-            ),
-            const SizedBox(
-              width: 30,
-            ),
-            FloatingActionButton(
-              heroTag: 'Next',
-              onPressed: () {
-                _pageController.nextPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInCubic,
-                );
-                _stepProgressController.nextStep();
-              },
-              tooltip: 'Next',
-              child: const Text('Next'),
             ),
           ],
         ),
